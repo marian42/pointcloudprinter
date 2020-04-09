34f9d259c67db23d26a460c92a8b534b48fed2db
Marian Kleineberg <mail@marian42.de>
2020-04-09 22:52:50 +0200

    Update Blender script to work with Blender 2.82

--- intersect.py
+++ intersect.py
@@ -15,13 +15,14 @@ obj_a = bpy.context.selected_objects[0]
 bpy.ops.import_mesh.stl(filepath = file_b)
 obj_b = bpy.context.selected_objects[0]
 
-bpy.context.scene.objects.active = obj_a
+bpy.context.view_layer.objects.active = obj_a
 bpy.ops.object.modifier_add(type = 'BOOLEAN')
 bpy.context.object.modifiers[0].object = obj_b
+bpy.context.object.modifiers[0].operation = 'INTERSECT'
 bpy.ops.object.modifier_apply(modifier='Boolean')
 
-bpy.context.scene.objects.active = obj_b
+bpy.context.view_layer.objects.active = obj_b
 bpy.ops.object.delete()
 
-bpy.context.scene.objects.active = obj_a
+bpy.context.view_layer.objects.active = obj_a
 bpy.ops.export_mesh.stl(filepath = file_out)
