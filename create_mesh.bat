cc71dc9ea4e68ce519639c03f84f4a959d1a532d
Marian Kleineberg <mail@marian42.de>
2020-04-09 22:42:49 +0200

    Update Meshlab script to work with the latest version of Meshlab

--- create_mesh.bat
+++ create_mesh.bat
@@ -28,7 +28,7 @@ pointcloudtool.exe extract %datadirectory% pointcloud.xyz %latitude% %longitude%
 
 pointcloudtool.exe fix pointcloud.xyz heightmap.xyz || goto :error
 
-%meshlab% -i pointcloud.xyz -i heightmap.xyz -o mesh.stl -s filter_script.mlx || goto :error
+%meshlab% -i pointcloud.xyz heightmap.xyz -o mesh.stl -s filter_script.mlx || goto :error
 
 pointcloudtool.exe makeSolid mesh.stl mesh.stl cube.stl %size% %verticaloffset% || goto :error
 
